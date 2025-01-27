198fac1a83cf90cae6634ac38ea7d9bea8aae991
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-01-27 21:28:19 +0500

    Add plot_hist and plot_image methods

---
+++ histogram generator.py
@@ -0,0 +1,27 @@
+import cv2
+import pylab
+from skimage import img_as_ubyte
+import numpy as np
+
+def plot_image(imag, title=''):
+    pylab.title(title, size=20), pylab.imshow(imag)
+    pylab.axis('off') # comment this line if you want axis ticks
+
+def plot_hist(r, title=''):
+    r = img_as_ubyte(r)
+    pylab.hist(np.array(r).ravel(), bins=256, range=(0, 256), color='r', alpha=0.5) 
+    pylab.xlabel('pixel value', size=20), pylab.ylabel('frequency', size=20)
+    pylab.title(title, size=20)
+
+def main():
+    path=input("Enter path to an image: ")
+    im = cv2.imread(path)
+    gray = im #cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    pylab.style.use('ggplot') 
+    pylab.figure(figsize=(15, 5))
+    pylab.subplot(121), plot_image(gray, 'original image')
+    pylab.subplot(122), plot_hist(gray, 'histogram for RGB channels')
+    pylab.show() 
+
+if __name__=="__main__":
+    main()
