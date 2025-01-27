8ae13d698d01e95dac39bd1abaf863e86436a9f9
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-01-27 21:29:56 +0500

    Add clear screen before run

--- histogram generator.py
+++ histogram generator.py
@@ -2,6 +2,7 @@ import cv2
 import pylab
 from skimage import img_as_ubyte
 import numpy as np
+import os
 
 def plot_image(imag, title=''):
     pylab.title(title, size=20), pylab.imshow(imag)
@@ -24,4 +25,8 @@ def main():
     pylab.show() 
 
 if __name__=="__main__":
+    if os.name=='nt':
+        os.system('cls')
+    else:
+        os.system('clear')
     main()
